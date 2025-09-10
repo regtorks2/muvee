0063237a4d3b25d4bbdf6e96b9d4592a040e7d6d
regtorks2 <regtorks1@gmail.com>
2025-09-10 14:39:29 +0000

    Sign In UI completed

---
+++ app/src/main/java/com/example/myapplicationnew/SignIn.kt
@@ -0,0 +1,11 @@
+package com.example.myapplicationnew
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SignIn : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.sign_in)
+    }
+}
