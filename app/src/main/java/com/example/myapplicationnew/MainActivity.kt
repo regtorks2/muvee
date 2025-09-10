0d1f81d34a31d44a24a42397bda44d6ed3715c52
regtorks2 <regtorks1@gmail.com>
2025-09-10 15:36:53 +0000

    Navigation completed for Sign in

--- app/src/main/java/com/example/myapplicationnew/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationnew/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.myapplicationnew
 
-import android.annotation.SuppressLint
+
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -10,6 +13,13 @@ class MainActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity)
 
+        val btnSignIn: Button = findViewById(R.id.sign_in)
+        btnSignIn.setOnClickListener{
+            val intent = Intent(this, SignIn::class.java)
+             startActivity(intent)
+          }
+
+        }
+
     }
 
-}
